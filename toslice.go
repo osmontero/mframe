df81279a1328d679f2ad5126d4fd86b4dc9b33d5
Osmany Montero <osmontero@icloud.com>
2023-03-22 15:34:52 +0200

    renaming variables

--- toslice.go
+++ toslice.go
@@ -18,26 +18,26 @@ func (d *DataFrame) ToSlice() []map[string]interface{} {
 }
 
 func (d *DataFrame) SliceOf(field string) ([]interface{}, *rerror.Error) {
-	var tmpList []interface{}
+	var list []interface{}
 	for _, v := range d.Data {
 		value, ok := v[field]
 		if !ok {
 			return []interface{}{}, rerror.ErrorF(http.StatusBadRequest, codes.InvalidArgument, "field '%s' not found in log '%v'", field, v)
 		}
-		tmpList = append(tmpList, value)
+		list = append(list, value)
 	}
 
-	return tmpList, nil
+	return list, nil
 }
 
 func (d *DataFrame) SliceOfFloat64(field string) ([]float64, *rerror.Error) {
-	tmpList, e := d.SliceOf(field)
+	list, e := d.SliceOf(field)
 	if e != nil {
 		return []float64{}, e
 	}
 
 	var fList = []float64{}
-	for _, value := range tmpList {
+	for _, value := range list {
 		v, ok := value.(float64)
 		if !ok {
 			return []float64{}, rerror.ErrorF(http.StatusBadRequest, codes.InvalidArgument, "'%v' is not type of float64", value)
